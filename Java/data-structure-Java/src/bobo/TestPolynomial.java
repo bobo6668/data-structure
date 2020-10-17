6bd8d92428ec7638a68b928fd375ede4f69379b9
bobo6668 <boboxie966668@gmail.com>
2020-10-17 21:09:22 +0800

    完成多项式乘法的编程

--- Java/data-structure-Java/src/bobo/TestPolynomial.java
+++ Java/data-structure-Java/src/bobo/TestPolynomial.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class TestPolynomial {
     public static void main(String[] args) {
+        // 读取第1个多项式
         Polynomial p1 = new Polynomial();
         p1.read();
         System.out.println(p1.toString());
@@ -13,31 +14,46 @@ public class TestPolynomial {
 //        p1.attach(scanner.nextInt(), scanner.nextInt());
 //        System.out.println(p1.toString());
 
+        // 读取第2个多项式
         Polynomial p2 = new Polynomial();
         p2.read();
         System.out.println(p2.toString());
 
         TestPolynomial testPolynomial = new TestPolynomial();
-        Polynomial p3 = testPolynomial.add(p1, p2);
-        System.out.println(p3.toString());
+        // 多项式相乘
+        Polynomial pMultiply = testPolynomial.multiply(p1, p2);
+        System.out.println(pMultiply.toString());
+        // 多项式相加
+        Polynomial pAdd = testPolynomial.add(p1, p2);
+        System.out.println(pAdd.toString());
     }
     public Polynomial add (Polynomial p1, Polynomial p2){
-        Polynomial pResult = p1;
+        Polynomial pResult = new Polynomial();
+        pResult.copy(p1); // 先深拷贝p1到pResult
 
         // 两个多项式相加，按照指数递降形式
-        int i = 0;
-        while (i < p2.n) {
+        for (int i = 0; i < p2.n; i++) {
             pResult.attach(p2.coef.get(i), p2.expo.get(i));
-            i++;
+        }
+        return pResult;
+    }
+    public Polynomial multiply (Polynomial p1, Polynomial p2){
+        Polynomial pResult = new Polynomial();
+
+        // 两个多项式相加，按照指数递降形式
+        for (int i = 0; i < p1.n; i++) {
+            for (int j = 0; j < p2.n; j++) {
+                pResult.attach(p1.coef.get(i) * p2.coef.get(j), p1.expo.get(i) + p2.expo.get(j));
+            }
         }
         return pResult;
     }
 }
 
-class Polynomial {
-    int n;
-    ArrayList<Integer> coef = new ArrayList<>();
-    ArrayList<Integer> expo = new ArrayList<>();
+class Polynomial { // 类：多项式
+    int n; // 项数
+    ArrayList<Integer> coef = new ArrayList<>(); // 系数（动态数组）
+    ArrayList<Integer> expo = new ArrayList<>(); // 指数（动态数组）
 
     public Polynomial() {
     }
@@ -53,37 +69,71 @@ class Polynomial {
     }
 
     public void attach(int coef, int expo) { // 按照指数递降形式，插入新的一项
-        for (int i = 0; i < n; i++) {
-            int thisExpo = this.expo.get(i);
-            int thisCoef = this.coef.get(i);
+        if (this.n == 0) {
+            this.coef.add(coef);
+            this.expo.add(expo);
+            n++;
+        }
+        else {
+            for (int i = 0; i < n; i++) {
+                int thisExpo = this.expo.get(i);
+                int thisCoef = this.coef.get(i);
 
-            if ( thisExpo == expo) { // 如果指数相等
-                int tmp = thisCoef + coef; // 系数相加
-                if (tmp != 0) {  // 如果系数相加不为0，则更新系数即可
-                    this.coef.set(i, tmp);
+                if ( thisExpo == expo) { // 如果指数相等
+                    int tmp = thisCoef + coef; // 系数相加
+                    if (tmp != 0) {  // 如果系数相加不为0，则更新系数即可
+                        this.coef.set(i, tmp);
+                    }
+                    else {  // 如果系数相加为0，则要删掉这一项
+                        this.coef.remove(i);
+                        this.expo.remove(i);
+                    }
+                    break; // 别忘了要break
                 }
-                else {  // 如果系数相加为0，则要删掉这一项
-                    this.coef.remove(i);
-                    this.expo.remove(i);
+                else if (thisExpo < expo) { // 如果当前指数小于输入的指数，因为是指数递降形式，所以需要插入新的一项
+                    this.coef.add(i, coef);
+                    this.expo.add(i, expo);
+                    n++;
+                    break;
                 }
-                break; // 别忘了要break
-            }
-            else if (thisExpo < expo) { // 如果当前指数小于输入的指数，因为是指数递降形式，所以需要插入新的一项
-                this.coef.add(i, coef);
-                this.expo.add(i, expo);
-                n++;
-                break;
+                else if (i == n-1) { // 如果输入的指数比已有的所有指数都小，需要插入到最后面
+                    this.coef.add(coef);
+                    this.expo.add(expo);
+                    n++;
+                    break;
+                }
             }
         }
     }
 
     @Override
     public String toString() {
-        return "Polynomial{" +
-                "n=" + n +
-                ", coef=" + coef +
-                ", expon=" + expo +
-                '}';
+//        return "Polynomial{" +
+//                "n=" + n +
+//                ", coef=" + coef +
+//                ", expon=" + expo +
+//                '}';
+        // 按照规定格式输出：
+        // 输出分2行，分别以指数递降方式输出乘积多项式以及和多项式非零项的系数和指数。
+        // 数字间以空格分隔，但结尾不能有多余空格。
+        // 零多项式应输出0 0。
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                sb.append(' ');
+            }
+            sb.append(this.coef.get(i)).append(' ').append(this.expo.get(i));
+        }
+        return sb.toString();
+    }
+
+    // 自己写一个拷贝函数
+    protected void copy(Polynomial pOld) {
+        this.n = pOld.n;
+        for (int i = 0; i < this.n; i++) {
+            this.coef.add(pOld.coef.get(i));
+            this.expo.add(pOld.expo.get(i));
+        }
     }
 }
 
