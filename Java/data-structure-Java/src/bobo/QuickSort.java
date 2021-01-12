aa7360c3de0c89a821b544a7845d1766a93d44fe
bobo6668 <boboxie966668@gmail.com>
2021-01-12 21:40:43 +0800

    再写一遍快排，熟练了

---
+++ Java/data-structure-Java/src/bobo/QuickSort.java
@@ -0,0 +1,40 @@
+package bobo;
+
+import dsa.Queue;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 0, 8, 6, 7, 19, 11};
+        System.out.println(Arrays.toString(nums));
+
+        QuickSort quickSort = new QuickSort();
+        quickSort.sort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public void sort(int[] nums, int left, int right) {
+        if (left < right) { //¡¾¡Ì¡¿
+            int mid = partition(nums, left, right);
+            sort(nums, left, mid - 1);
+            sort(nums, mid + 1, right);
+        }
+    }
+    private int partition(int[] nums, int left, int right) {
+        int value = nums[left]; //¡¾¡Ì¡¿
+        int i = left, j = right; //¡¾¡Ì¡¿
+        while (i < j) { //¡¾¡Ì¡¿
+            while (nums[j] >= value && j > i) j--; //¡¾¡Ì¡¿
+            while (nums[i] <= value && i < j) i++;
+            if (i < j) swap(nums, i, j);
+        }
+        swap(nums, left, i); //¡¾¡Ì¡¿
+        return i;
+    }
+    private void swap(int[] arr, int i, int j) { //¡¾¡Ì¡¿
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
